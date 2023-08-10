0736b0c4d33e0167b92cdfa9a6c943192f448cf5
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-08-10 15:28:13 +0000

    Deploying to gh-pages from @ Zhihare/goit-js-hw-10@11d39485ab1e71c1f0352601ca2339ed6f04b8a0 🚀

---
+++ 01-color-switcher.5461ccad.js
@@ -0,0 +1,2 @@
+!function(){var t=null,e={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")};e.stop.disabled="true",e.start.addEventListener("click",(function(){t=setInterval((function(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))),e.start.disabled="true",e.stop.removeAttribute("disabled")}),1e3)})),e.stop.addEventListener("click",(function(){clearInterval(t),e.start.removeAttribute("disabled"),e.stop.disabled="true"}))}();
+//# sourceMappingURL=01-color-switcher.5461ccad.js.map
